a9ae0c1b34f8d3c810d16014a1cb5e5107ce8409
Erick Contreras <er.contreras@proton.me>
2026-09-09 15:48:11 -0600

    style headers background color and forground color

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
-const options = [_][]const u8{
-    "Add",
-    "Remove",
-    "Update",
-    "List All",
-    "Exit"
-};
+
+const options = [_][]const u8{ "Add", "Remove", "Update", "Exit" };
 
 const goals = [_][]const u8{
     "Meditation",
@@ -34,10 +29,11 @@ fn add_task() !void {
     print("Adding task...\n", .{});
 }
 
-fn list_all_task() !void {
-    print("List all tasks...\n", .{});
+fn list_all_task() void {
+    print("\n", .{});
+
     for (headers) |header| {
-        print("{s} | ", .{header});
+        print("\x1b[30;47m{s}\x1b[0m | ", .{header});
     }
 
     print("\n", .{});
@@ -55,15 +51,17 @@ pub fn main(init: std.process.Init) !void {
     var file_reader = stdin_file.reader(init.io, &input_buffer);
     const reader = &file_reader.interface;
 
-    print("Choose an option\n", .{});
-    for (options, 0..) |option, i| {
-        print("{d}. {s} a task\n", .{i + 1, option});
-    }
-
-    std.debug.print("Add a task and hit enter: \n", .{});
-
     var boolean = true;
     while (boolean) {
+        print("Choose an option\n", .{});
+        for (options, 0..) |option, i| {
+            print("{d}. {s} a task | ", .{ i + 1, option });
+        }
+
+        print("\n", .{});
+
+        list_all_task();
+
         if (try reader.takeDelimiter('\n')) |line| {
             const number: u8 = std.fmt.parseInt(u8, line, 10) catch |err| switch (err) {
                 error.InvalidCharacter => {
@@ -80,8 +78,7 @@ pub fn main(init: std.process.Init) !void {
                 1 => try add_task(),
                 2 => print("Removing task...\n", .{}),
                 3 => print("Updating task...\n", .{}),
-                4 => try list_all_task(),
-                5 => boolean = false,
+                4 => boolean = false,
                 else => print("Unexpected number try again\n", .{}),
             }
         }
